683507d6abbf832d0684abef62eb744e1b1445f2
Betty <betty.meg356@gmail.com>
2022-07-13 23:20:53 +0800

    updated email

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 launchMailto() async {
   final mailtoLink = Mailto(
-    to: ['to@example.com'],
+    to: ['betty.meg356@gmail.com'],
     cc: ['cc1@example.com', 'cc2@example.com'],
     subject: 'mailto example subject',
     body: 'mailto example body',
